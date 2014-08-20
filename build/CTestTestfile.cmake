04e0ec69f4b5e01b9b3c49905b081b25558e5c7c
Mohit Shridhar <mohit.appi@gmail.com>
2014-08-20 22:24:07 +0800

    Removing build directory

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/MohitSridhar/ros_pkgs/src/gui_teleop
-# Build directory: /Users/MohitSridhar/ros_pkgs/src/gui_teleop/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
